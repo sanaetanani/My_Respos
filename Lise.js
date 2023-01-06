3fd89299ba368d4566fe8d3fad00cb87a45b3a7b
sanaetanani <116953836+sanaetanani@users.noreply.github.com>
2023-01-06 21:44:49 +0100

    Update Lise.js

--- Lise.js
+++ Lise.js
@@ -1,7 +1,7 @@
 var t = [];
 function enregistre(){
     var p = {
-        "Ref":        document.getElementById("ref").value ,
+        "Ref":        document.getElementById("ref").value,
         "Designation":document.getElementById("designation").value,
         "Prix":       document.getElementById("prix").value,
         "Categorie":  document.getElementById("categorie").value,
