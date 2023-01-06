631aea1514b651bd6763bac8fb7304b5e7c62778
Yassine-ech <88655936+yassine-ech@users.noreply.github.com>
2023-01-06 12:08:31 +0100

    Update Lise.js

--- Lise.js
+++ Lise.js
@@ -6,24 +6,4 @@ function enregistre(){
         "Prix":       document.getElementById("prix").value,
         "Categorie":  document.getElementById("categorie").value,
     }
-    t.push(p);
-    console.log(t);
-}
-function afficher(){
-    document.getElementById('produits').innerHTML= ""
-    for(var p of t){
-        document.getElementById('produits').innerHTML+='<tr><td>'+ p.Ref +'</td><td>' + p.Designation + '</td><td>' + p.Prix + '</td><td>' + p.Categorie + '</td><td><button type="button" onclick=supp("'+p.Ref+'")>supprimer</button></td></tr>'
-       
-        
-    } 
-}
-function supp(ref){
-    for(var i=0;i<t.length;i++){
-        if(t[i].Ref==ref){
-            t.splice(i,1);
-            break;
-        }
-    }
-    afficher();
-    console.log(t);
-}
+    
