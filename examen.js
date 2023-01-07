d1d925083bcb08755cb5f23ee69879e4257a76e5
karimbennor <122154209+karimbennor@users.noreply.github.com>
2023-01-07 12:05:34 +0100

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,14 @@
+function afficherCacher(etat){
+  document.getElementById("mahram").style.display=etat;
+}
+function calculDuree(){
+  var date1= new Date(document.getElementById('par2').value);
+  var date2= new Date();
+  var duree= date2.getFullYear() - date1.getFullYear();
+  if(duree<9){
+    alert("vous n'avez pas encore le droit ")
+  }
+  else{
+    alert('inscription ok')
+  }  
+}
