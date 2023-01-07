c3cd763bef85bca2e4c8b19d14ccf15e009e640e
sanaetanani <116953836+sanaetanani@users.noreply.github.com>
2023-01-07 12:07:09 +0100

    Update examen.js

--- examen.js
+++ examen.js
@@ -5,7 +5,7 @@ function calculDuree(){
   var date1= new Date(document.getElementById('par2').value);
   var date2= new Date();
   var duree= date2.getFullYear() - date1.getFullYear();
-  if(duree<9){
+  if(duree<9){     
     alert("vous n'avez pas encore le droit ")
   }
   else{
